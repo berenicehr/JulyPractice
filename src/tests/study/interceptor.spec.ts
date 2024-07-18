d46e8d9130e3f1ecfe29604a4b71ea05eb75b65f
Berenice Huerta <berenice_huerta@epam.com>
2024-07-18 15:58:06 -0600

    feat: interceptor usage

---
+++ src/tests/study/interceptor.spec.ts
@@ -0,0 +1,29 @@
+import {test, expect} from "@playwright/test"
+import { Loginpage } from "../../po/pages/loginpage";
+
+test("Interceptor usage", async({page}) => {
+
+    // Agilizar la ejecucion de un test, removiendo la carga de ciertos elementos
+    // En llamados api, no es necesario la carga de las imagenes 
+    
+    await page.on("request", req => {
+        console.log(req.url());
+    })
+    
+    await page.route("https://www.saucedemo.com/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg", 
+        (route) => route.abort())
+    
+    await page.route("https://www.saucedemo.com/static/media/bike-light-1200x1500.37c843b0.jpg", 
+            (route) => route.abort())
+
+    // Para evitar la carga de ciertas imagenes
+
+    await page.goto("https://www.saucedemo.com/");
+    
+    const loginPage = new Loginpage(page);
+    await loginPage.loginWithCredentials("standard_user", "secret_sauce");
+
+     // Crear screenshot de pantalla y guardarla con nombre y localizacion 
+
+    await page.screenshot({path: "intercepted.png", fullPage: true})
+}) 
